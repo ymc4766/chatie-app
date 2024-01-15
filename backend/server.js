e2e2b3c6da17079796fa082b7e9f36575f4a83e3
yusto <yuusufmahud@gmail.com>
2024-01-15 22:54:54 +0300

    cmmtee

--- backend/server.js
+++ backend/server.js
@@ -69,23 +69,23 @@ app.use("/api", messageRoutes);
 
 // app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static("/var/data/uploads"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
 
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) => {
-//     // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// } else {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-//   app.get("/", (req, res) => {
-//     res.send("API is running....");
-//   });
-// }
+  app.get("*", (req, res) => {
+    // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use("/*", handleNotFound);
 // app.use(serverError);
