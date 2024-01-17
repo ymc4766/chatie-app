302545afc6887777f000958e4165003f9eb42441
yusto <yuusufmahud@gmail.com>
2024-01-17 23:13:27 +0300

    serve

--- backend/server.js
+++ backend/server.js
@@ -70,14 +70,10 @@ app.use("/api", messageRoutes);
 // app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  // app.use("/uploads", express.static("/var/data/uploads"));
-
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
   });
 } else {
   const __dirname = path.resolve();
