66fadac0e939c7c3665dd475434814dac9f6cd1b
Ymc <67848669+ymc4766@users.noreply.github.com>
2024-02-24 12:58:56 +0300

    Update SocketServer.js

--- backend/SocketServer.js
+++ backend/SocketServer.js
@@ -3,15 +3,7 @@ export default function (socket, io) {
   // user joins or opens app
 
   socket.on("join", (user) => {
-    socket.join(user);
-    //add joined user to online users
-    if (!onlineUsers.some((u) => u.userId === user)) {
-      onlineUsers.push({ userId: user, socketId: socket.id });
-    }
-    //send online users to frontend
-    io.emit("get-online-users", onlineUsers);
-    //send socket id
-    io.emit("setup socket", socket.id);
+   .... Code here 
   });
 
   //socket ofline - user
@@ -35,8 +27,7 @@ export default function (socket, io) {
     let conversation = message.conversation;
     if (!conversation.users) return;
     conversation.users.forEach((user) => {
-      if (user._id === message.sender?._id) return;
-      socket.in(user._id).emit("messageRecieve", message);
+// code Here 
     });
   });
 
@@ -55,10 +46,7 @@ export default function (socket, io) {
     let userId = data.userToCall;
     let userSocketId = onlineUsers.find((user) => user.userId == userId);
     io.to(userSocketId?.socketId).emit("call user", {
-      signal: data.signal,
-      from: data.from,
-      name: data.name,
-      picture: data.picture,
+     // Code here ///
     });
   });
   //answer call
